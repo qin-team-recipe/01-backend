5e417fcb63e4acd9b0f024fdc26f6346e02afc9b
TAKO-zhangyu <takoforme@gmail.com>
2023-06-24 00:10:00 +0900

    rubocopの指摘修正

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,6 @@
 class Chef < ApplicationRecord
-  has_many :recipes, as: :author
-  has_many :favorite_chefs
+  has_many :recipes, as: :author, dependent: :destroy
+  has_many :favorite_chefs, dependent: :destroy
   has_many :users, through: :favorite_chefs
-  has_many :external_links
+  has_many :external_links, dependent: :destroy
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
-  belongs_to :author, polymorphic: true
-  has_many :favorite_recipes
+  belongs_to :author, polymorphic: true, dependent: :destroy
+  has_many :favorite_recipes, dependent: :destroy
   has_many :users, through: :favorite_recipes
-  has_many :steps
-  has_many :materials
-  has_many :cart_lists
+  has_many :steps, dependent: :destroy
+  has_many :materials, dependent: :destroy
+  has_many :cart_lists, dependent: :destroy
 end
