528f8ff2a90d171dd0dbc6cf1de5343b7a3b1bc6
Kazuki <shanbenkazuki15@gmail.com>
2023-06-23 23:39:30 +0900

    null制約を追加

--- db/migrate/20230623081912_create_cart_lists.rb
+++ db/migrate/20230623081912_create_cart_lists.rb
@@ -3,8 +3,8 @@ class CreateCartLists < ActiveRecord::Migration[7.0]
     create_table :cart_lists do |t|
       t.references :recipe, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.string :name
-      t.integer :position
+      t.string :name, null: false
+      t.integer :position, null: false
 
       t.timestamps
     end
