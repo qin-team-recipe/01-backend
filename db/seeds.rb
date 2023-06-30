13c1658532c801ddc81aad7d6c694cd5042b33f1
Kazuki <shanbenkazuki15@gmail.com>
2023-06-30 22:34:18 +0900

    seedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Faker::Config.locale = :ja
+
+10.times do
+  user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    crypted_password: Faker::Internet.password,
+    salt: Faker::Crypto.md5
+  )
+
+  chef = Chef.create!(
+    name: Faker::Name.name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    thumbnail: Faker::Avatar.image(slug: "my-own-slug", size: "50x50")
+  )
+
+  external_link = ExternalLink.create!(
+    chef_id: chef.id,
+    title: Faker::Lorem.sentence(word_count: 3),
+    url: Faker::Internet.url,
+    link_type: ['Twitter', 'Instagram', 'Website'].sample,
+    follower_count: rand(1000..10000)
+  )
+
+  favorite_chef = FavoriteChef.create!(
+    user_id: user.id,
+    chef_id: chef.id
+  )
+
+  3.times do
+    author_type = ['User', 'Chef'].sample
+    author_id = author_type == 'User' ? user.id : chef.id
+
+    recipe = Recipe.create!(
+      name: Faker::Food.dish,
+      description: Faker::Food.description,
+      thumbnail: Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
+      serving_size: rand(1..5),
+      is_draft: [true, false].sample,
+      author_type: author_type,
+      author_id: author_id
+    )
+
+    material = Material.create!(
+      recipe_id: recipe.id,
+      name: Faker::Food.ingredient,
+      memo: Faker::Food.description
+    )
+
+    step = Step.create!(
+      recipe_id: recipe.id,
+      title: Faker::Lorem.sentence(word_count: 3),
+      memo: Faker::Food.description
+    )
+
+    favorite_recipe = FavoriteRecipe.create!(
+      user_id: user.id,
+      recipe_id: recipe.id
+    )
+
+    cart_list = CartList.create!(
+      recipe_id: recipe.id,
+      user_id: user.id,
+      name: Faker::Food.dish,
+      position: rand(1..10)
+    )
+
+    cart_item = CartItem.create!(
+      cart_list_id: cart_list.id,
+      name: Faker::Food.ingredient,
+      memo: Faker::Food.description,
+      is_checked: [true, false].sample
+    )
+  end
+end
