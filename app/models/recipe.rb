20e9392369603a23c925ad6478461c39ec1ac5d0
natsuki <su811sh1@gmail.com>
2023-07-05 11:24:28 +0900

    update: recipeとuserのリレーションを修正 #35

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,5 @@ class Recipe < ApplicationRecord
   has_many :users, through: :favorite_recipes
   has_many :steps, dependent: :destroy
   has_many :materials, dependent: :destroy
-  has_many :cart_lists, dependent: :destroy
+  has_one :cart_lists, dependent: :destroy
 end
