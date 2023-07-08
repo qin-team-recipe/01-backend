80add9e1061bcbbd48434029041470605a28bfb7
natsuki <su811sh1@gmail.com>
2023-07-08 12:10:16 +0900

    updat: 無視したいRubocopの指摘をrubocop.ymlファイルに記載してしまっていたので、コメントで無効化する形に修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }  # rubocop:disable all
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root.join('spec/fixtures')}"
+  config.fixture_path = "#{Rails.root.join('spec/fixtures')}"  # rubocop:disable all
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
