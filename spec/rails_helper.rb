ebad5b09a89c386894fc974b83800f7a03c2c8b5
natsuki <su811sh1@gmail.com>
2023-07-10 10:10:25 +0900

    update: gem:'shoulda-matchers'を導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,3 +67,10 @@ RSpec.configure do |config|
   # FactoryBotのシンタックスを省略できるようにする
   config.include FactoryBot::Syntax::Methods
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
