f105ebf2b7ce0a9528fa5918616ae4772fd1b334
natsuki <su811sh1@gmail.com>
2023-08-13 17:57:19 +0900

    update: Rubocop指摘の修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,4 @@
 class Relationship < ApplicationRecord
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followed, class_name: 'User'
+  belongs_to :follower, class_name: 'User', inverse_of: :active_relationships
+  belongs_to :followed, class_name: 'User', inverse_of: :passive_relationships
 end
-
--- spec/models/recipe_external_link_spec.rb
+++ spec/models/recipe_external_link_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe RecipeExternalLink, type: :model do
+RSpec.describe RecipeExternalLink do
   pending "add some examples to (or delete) #{__FILE__}"
 end
