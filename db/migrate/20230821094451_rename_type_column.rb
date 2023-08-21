eb9b552305e7d13081e4eca6e0f6f98e1b82a7e6
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-21 19:31:16 +0900

    roleではなくuser_typeに変更

--- db/migrate/20230821094451_rename_type_column.rb
+++ db/migrate/20230821094451_rename_type_column.rb
@@ -1,6 +1,6 @@
 class RenameTypeColumn < ActiveRecord::Migration[7.0]
   def change
-    rename_column :users, :type, :role
+    rename_column :users, :type, :user_type
     rename_column :recipe_external_links, :type, :url_type
     rename_column :user_external_links, :type, :url_type
   end
