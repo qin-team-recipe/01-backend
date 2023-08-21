1059265599911f9cb44d755bf07a61f739d4da87
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-21 18:46:59 +0900

    各テーブルのカラム名をtypeから変更

---
+++ db/migrate/20230821094451_rename_type_column.rb
@@ -0,0 +1,7 @@
+class RenameTypeColumn < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :type, :role
+    rename_column :recipe_external_links, :type, :url_type
+    rename_column :user_external_links, :type, :url_type
+  end
+end
