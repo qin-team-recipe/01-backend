7dc37f3884ab9b98d169a880a7421f624710cab3
Natsuki Hayashi <114204669+darutani@users.noreply.github.com>
2023-08-26 11:09:27 +0900

    Update app/models/user.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   # 名前系：100文字以内、説明系：256文字以内で一旦設定
   validates :name, presence: true, length: { maximum: 100 }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email format' }
   validates :domain, presence: true, uniqueness: true
   validates :user_type, presence: true, inclusion: { in: ['user', 'chef'] }
   validates :description, length: { maximum: 256 }
