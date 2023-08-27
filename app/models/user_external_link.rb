0edc718a74775830fd8597009bff769f8f9a4b09
TAKO-zhangyu <takoforme@gmail.com>
2023-08-27 17:23:48 +0900

    impl: UserExternalLinkモデルのバリデーションを追加

--- app/models/user_external_link.rb
+++ app/models/user_external_link.rb
@@ -1,3 +1,16 @@
 class UserExternalLink < ApplicationRecord
   belongs_to :user
+
+  URL_TYPES = {
+    youtube: 'YouTube',
+    instagram: 'Instagram',
+    tiktok: 'TikTok',
+    twitter: 'Twitter',
+    facebook: 'Facebook'
+  }.freeze
+
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'のフォーマットが正しくありません' }
+  validates :user_id, uniqueness: { scope: :url_type }, allow_nil: true, unless: -> { url_type.nil? }
+  validates :user_id, uniqueness: { scope: :url }
+  validates :url_type, inclusion: { in: URL_TYPES.values }, allow_nil: true
 end
