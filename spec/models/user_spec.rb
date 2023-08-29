f46871b67eb319f596ff4fdb2abce46771971650
natsuki <su811sh1@gmail.com>
2023-08-29 09:29:26 +0900

    update: Rubocopの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
   describe 'validations' do
-    it { should validate_inclusion_of(:user_type).in_array(['user', 'chef']) }
+    it { is_expected.to validate_inclusion_of(:user_type).in_array(['user', 'chef']) }
   end
 end
