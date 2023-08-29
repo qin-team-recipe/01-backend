5a5636dd9593b66babb901079e4795b9fbbb8748
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-29 18:00:52 +0900

    バリデーションを追加

--- app/models/cart_list.rb
+++ app/models/cart_list.rb
@@ -2,4 +2,8 @@ class CartList < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
   has_many :cart_items, dependent: :destroy
+
+  validates :name, presence: true
+  validates :position, presence: true, numericality: { only_integer: true }
+  validates :own_notes, inclusion: { in: [true, false] }
 end
