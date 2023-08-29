ebfd638174332aa46583c78ecf50411624899cbe
natsuki <su811sh1@gmail.com>
2023-08-29 09:10:07 +0900

    update: userモデルのバリデーションを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe User
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_inclusion_of(:user_type).in_array(['user', 'chef']) }
+  end
+end
