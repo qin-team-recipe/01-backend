e729d1213ab4b3fc567c8f52ed677e3f34cb252b
TAKO-zhangyu <takoforme@gmail.com>
2023-08-31 03:44:42 +0900

    impl: cart_listsを取得するエンドポイントを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace 'api', defaults: { format: 'json' } do
     namespace 'v1' do
       resources :recipes
+
+      resources :users do
+        resources :carts, only: %i[index], controller: 'cart_lists'
+      end
     end
   end
 end
