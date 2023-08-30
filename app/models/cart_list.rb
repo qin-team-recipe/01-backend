4deb51e366c883478f4aec1f567768737f195d46
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-08-30 15:37:37 +0900

    nameに最大文字数のバリデーションを設定

--- app/models/cart_list.rb
+++ app/models/cart_list.rb
@@ -3,7 +3,7 @@ class CartList < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 100 }
   validates :position, presence: true, numericality: { only_integer: true }
   validates :own_notes, inclusion: { in: [true, false] }
 end
