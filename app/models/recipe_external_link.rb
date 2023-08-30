ad848e6ca01754bd40f8ee991c3d25c535a013af
TAKO-zhangyu <takoforme@gmail.com>
2023-08-30 09:37:14 +0900

    fix: 不要なallow_nilオプションを削除

--- app/models/recipe_external_link.rb
+++ app/models/recipe_external_link.rb
@@ -4,7 +4,7 @@ class RecipeExternalLink < ApplicationRecord
   belongs_to :recipe
 
   validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'のフォーマットが正しくありません' }
-  validates :recipe_id, uniqueness: { scope: :url_type }, allow_nil: true, unless: -> { url_type.nil? }
+  validates :recipe_id, uniqueness: { scope: :url_type }, unless: -> { url_type.nil? }
   validates :recipe_id, uniqueness: { scope: :url }
   validates :url_type, inclusion: { in: URL_TYPES.values }, allow_nil: true
 end
--- app/models/user_external_link.rb
+++ app/models/user_external_link.rb
@@ -4,7 +4,7 @@ class UserExternalLink < ApplicationRecord
   belongs_to :user
 
   validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'のフォーマットが正しくありません' }
-  validates :user_id, uniqueness: { scope: :url_type }, allow_nil: true, unless: -> { url_type.nil? }
+  validates :user_id, uniqueness: { scope: :url_type }, unless: -> { url_type.nil? }
   validates :user_id, uniqueness: { scope: :url }
   validates :url_type, inclusion: { in: URL_TYPES.values }, allow_nil: true
 end
