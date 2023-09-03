cbb24e6726673a9c0741c31df9dc533b4a52cf43
TAKO-zhangyu <takoforme@gmail.com>
2023-09-03 13:42:14 +0900

    impl: recipe/:idのエンドポイントを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api', defaults: { format: 'json' } do
     namespace 'v1' do
-      resources :recipes
+      resources :recipes, only: %i[index show]
 
       resources :users do
         resources :cart_lists, only: %i[index]
