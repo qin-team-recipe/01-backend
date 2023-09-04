321fca33b11945d19d4fc0d396f6c3e0f805e5ef
TAKO-zhangyu <takoforme@gmail.com>
2023-09-04 23:46:39 +0900

    impl: ユーザー人気レシピのエンドポイントを実装

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -73,4 +73,37 @@ RSpec.describe 'Recipes' do
       end
     end
   end
+
+  describe 'GET /user/:user_id/popular_recipes' do
+    let(:user) { create(:user) }
+    let!(:recipe) { create(:recipe, user:) }
+
+    context 'レシピのレコードがあるとき' do
+      it '200を返却すること' do
+        get popular_recipes_api_v1_user_path(user)
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'レスポンスの中身は1件のみであること' do
+        get popular_recipes_api_v1_user_path(user)
+
+        expect(response.parsed_body.length).to eq 1
+      end
+
+      it 'recipeのレコードを返却すること' do
+        get popular_recipes_api_v1_user_path(user)
+
+        expect(response.parsed_body[0]).to include({
+                                                     'id' => recipe.id,
+                                                     'name' => recipe.name,
+                                                     'description' => recipe.description,
+                                                     'favorite_count' => recipe.favoriters_count,
+                                                     'thumbnail' => recipe.thumbnail,
+                                                     'chef_name' => recipe.user.name,
+                                                     'created_at' => recipe.created_at.iso8601(3),
+                                                     'updated_at' => recipe.updated_at.iso8601(3)
+                                                   })
+      end
+    end
+  end
 end
