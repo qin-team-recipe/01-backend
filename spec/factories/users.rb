e62e7819abba802c3e3bf475008680d9c47861d2
shanbenkazuki <shanbenkazuki15@gmail.com>
2023-09-08 13:17:26 +0900

    不必要なtraitを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,12 +11,6 @@ FactoryBot.define do
       user_type { 'chef' }
     end
 
-    trait :with_follower_users do
-      after(:create) do |user|
-        user.followers << create_list(:user, 5)
-      end
-    end
-
     trait :with_recipes do
       after(:create) do |user|
         create_list(:recipe, 5, user: user)
